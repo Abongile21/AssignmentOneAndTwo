ade29a87f812b8289e68d276fed6a4b6cbdcc076
Abongile21 <abongiletheo00@gmail.com>
2025-04-17 14:19:32 +0200

    modified code a bit

--- AssignmentOneAndTwo/Program.cs
+++ AssignmentOneAndTwo/Program.cs
@@ -4,54 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(
-                "********************************************\n" +
-                "*           Welcome to dev games!          *\n" +
-                "********************************************");
-            Console.ResetColor();
-
-            Console.WriteLine();
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("*** Pick game to play ***");
-            Console.WriteLine(" 1.Beer Song.");
-            Console.WriteLine(" 2.Numbers guess game.");
-            Console.ResetColor();
-            
-            string choice = Console.ReadLine();
-
-            if (int.Parse(choice) == 1)
+            while (true)
             {
-                Console.WriteLine("Enter the length of your song:");
-                string songLength = Console.ReadLine();
-                BeerSong beerSong = new BeerSong();
-                int number;
 
-                if(int.TryParse(songLength, out number)){
-                    beerSong.singBeerSong(int.Parse(songLength));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(
+                    "********************************************\n" +
+                    "*           Welcome to dev games!          *\n" +
+                    "********************************************");
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("*** Pick game to play ***");
+                Console.WriteLine(" 1.Beer Song.");
+                Console.WriteLine(" 2.Numbers guess game.");
+                Console.ResetColor();
+
+                string choice = Console.ReadLine();
+
+                if (int.Parse(choice) == 1)
+                {
+                    Console.WriteLine("Enter the length of your song:");
+                    string songLength = Console.ReadLine();
+                    BeerSong beerSong = new BeerSong();
+                    int number;
+
+                    if (int.TryParse(songLength, out number))
+                    {
+                        beerSong.singBeerSong(int.Parse(songLength));
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a number!");
+                    }
+
+
+                    //Do something
+                }
+                else if (int.Parse(choice) == 2)
+                {
+
+                    //Play 
+                    GuessTheNumber guessTheNumber = new GuessTheNumber();
+                    guessTheNumber.Play();
 
                 }
-                else{
-                    Console.WriteLine("Enter a number!");
+                else
+                {
+                    Console.WriteLine("Game not available at moment");
                 }
-                
 
-                //Do something
+
+                Console.WriteLine();
+
+                Console.WriteLine("***************THE END**********************");
+
+                Console.WriteLine("PLAY AGAIN OR PRESS 'CTRL + C' TO CLOSE GAME");
+                Console.WriteLine();
+
+
+
+
+
+
             }
-            else if (int.Parse(choice) == 2) {
 
-                //Play 
-                GuessTheNumber guessTheNumber = new GuessTheNumber();
-                guessTheNumber.Play();
-
-            }
-            else
-            {
-                Console.WriteLine("Game not available at moment");
-            }
-
-
-        }
-    }
+    }   }
 }
